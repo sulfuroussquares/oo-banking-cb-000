ebaa19068995af637654e24741d99a58b1a3a68f
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-10 00:13:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,8 +25,8 @@ if self.valid? && @status == "pending" && @sender.balance > @amount
   @receiver.balance += @amount
   @status = "complete"
 else
-  "Transaction rejected. Please check your account balance."
   @status = "rejected"
+  "Transaction rejected. Please check your account balance."
 end #endif
 end
 
