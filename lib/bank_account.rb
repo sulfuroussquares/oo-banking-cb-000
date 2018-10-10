896fbb955bf8a9f011285b5f232b8aa0fefe9114
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-10 00:01:50 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
+  attr_accessor :balance, :status
+
+  def name
+    @name
+  end
+
+def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = "open"
+end
+
+def deposit(value)
+  @balance += value
+end
+
+def display_balance
+   "Your balance is $#{@balance}."
+end
+
+
+def valid?
+  if self.status == "open" && self.balance > 0
+    true
+  else
+    false
+  end
+end
+
+def close_account
+  self.status = "closed"
+end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,33 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+def valid?
+  #The complicated way to validate bank accounts
+  #if @sender.status == "open" && @sender.balance > 0 && @receiver.status == "open" && @receiver.balance > 0
+  #The smooth way to validate bank accounts
+  if @sender.valid? && @receiver.valid?
+    true
+  else
+    false
+  end
+end
+
+def execute_transaction
+if self.valid? && @status == "pending"
+  @sender.balance -= @amount
+  @receiver.balance += @amount
+  @status = "complete"
+else
+  return "Transaction rejected. Please check your account balance."
+  @status = "rejected"
+end #endif
+end
+
 end
